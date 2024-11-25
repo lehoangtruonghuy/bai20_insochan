f0cec6a5162692860d93e5d884ac453da71988fd
MAY 06 <MAY 06@LAB2-17>
2024-11-25 16:32:06 +0700

    Add project files.

---
+++ bai20_insochan/bai20_insochan.cpp
@@ -0,0 +1,23 @@
+﻿//bai20_insochan
+#include <iostream>
+using namespace std;
+
+int main() {
+    int so, tongchan = 0, tongle = 0, tong = 0;
+    cout << "Nhap so n: "; cin >> so;
+    for (int i = 1; i <= so; i++) {
+        tong += i;
+        if (i % 2 == 0) {
+            cout << i << " ";
+            tongchan += i;
+        }
+        else {
+            tongle += i;
+        }
+    }
+
+    cout << "\n Tong cac so: " << tong;
+    cout << "\n Tobg cac so chan: " << tongchan;
+    cout << "\n Tobg cac so le: " << tongle;
+    return 0;
+}
